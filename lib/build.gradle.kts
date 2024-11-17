bb3c443fda14ceb9684cb50a1131aca6a1e7d72f
Regensburger Ervin Máté <ervin.regensburger@gmail.com>
2024-11-17 20:54:29 +0100

    Reduced build configuration redundancy

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,22 +1,3 @@
 plugins {
-    java
-}
-
-java.toolchain {
-    languageVersion.set(JavaLanguageVersion.of(21))
-}
-
-repositories {
-    mavenCentral()
-}
-
-val picoCliVersion = "4.7.6"
-val slf4jVersion = "1.7.36"
-val log4jVersion = "2.23.1"
-
-dependencies {
-    implementation("org.slf4j:slf4j-api:$slf4jVersion")
-    implementation("info.picocli:picocli:$picoCliVersion")
-
-    runtimeOnly("org.apache.logging.log4j:log4j-slf4j-impl:$log4jVersion")
+    id("hu.bme.mit.ase.shingler.gradle.java")
 }
